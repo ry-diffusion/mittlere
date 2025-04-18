e530abc02de97c52eab899574b5f8c9c35cf4bd1
Ryster <ry.diffusion@proton.me>
2025-04-17 22:41:19 -0300

    use swc

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import viteReactSwc from '@vitejs/plugin-react-swc'
+
 
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReactSwc(), tailwindcss()],
   test: {
     globals: true,
     environment: "jsdom",
